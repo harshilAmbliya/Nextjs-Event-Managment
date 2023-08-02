d0d416c2821218567119218924f53d62bff88126
Axit Vadi <axitvadi@gmail.com>
2023-08-02 20:38:45 +0530

    feat:allevents

--- pages/allevents.js
+++ pages/allevents.js
@@ -39,7 +39,7 @@ const AllEvents = () => {
     const [listing, setListing] = useState([]);
     const [loading, setLoading] = useState(false);
 
-    
+
     useEffect(() => {
         const fetchData = async () => {
             try {
@@ -59,11 +59,43 @@ const AllEvents = () => {
         <div>
             {loading && <div>Loading...</div>}
             {
-            listing &&listing.map(product => (
-                    <div key={product.id}>
-                        {product.name}
-                    </div>
-                ))
+                // listing &&listing.map(product => (
+                //         <div key={product.id}>
+                //             {product.name}
+                //         </div>
+                //     ))
+
+                listing && listing.map((product) => {
+                    const{name ,id ,date ,location ,slug} = product
+                    return (
+                        <div key={id} style={{
+                            color: 'white',
+                            backgroundColor: 'gray',
+                            padding: '10px',
+                            margin: '10px',
+                            borderRadius: '10px',
+                            textAlign: 'center',
+                            display: 'flex',
+                            flexDirection: 'column',
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                            width: '100%',
+                            height: '300px',
+                            boxShadow: '0 0 10px rgba(0, 0, 0, 0.5)',
+                            transition: 'all 0.3s ease-in-out',
+                            ':hover': 
+                            {
+                                boxShadow: '0 0 20px rgba(0, 0, 0, 0.5)',
+                                transform: 'translateY(-5px)',
+                            }
+                        }}>
+                           <h1>{name} </h1>
+                           <p>{date}</p>
+                           <h3>{slug}</h3>
+                           <p>{location} </p>
+                        </div>
+                    )
+                })
             }
         </div>
     )
