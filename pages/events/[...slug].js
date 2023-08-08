d715d83b9b2315dac1cddbd78c7ec0f28b9df569
harshilAmbliya <harshilambliya15@gmail.com>
2023-08-08 16:08:21 +0530

    feat : index page eventList

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,9 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react';
+import styles from './EventPage.module.css'; // Import the CSS module
 
-export function Slugpage() {
-    return (
-        <div>Filter event page</div>
-    )
-}
+const eventsData = [
+  { id: 1, title: 'Event 1', category: 'Concert' },
+  { id: 2, title: 'Event 2', category: 'Conference' },
+  { id: 3, title: 'Event 3', category: 'Exhibition' },
+  // Add more event data
+];
 
-export default Slugpage;
+const EventPage = () => {
+  const [selectedCategory, setSelectedCategory] = useState('All');
+
+  const handleCategoryChange = (category) => {
+    setSelectedCategory(category);
+  };
+
+  const filteredEvents = selectedCategory === 'All'
+    ? eventsData
+    : eventsData.filter(event => event.category === selectedCategory);
+
+  return (
+    <div className={styles['event-page']}>
+      <h1 className={styles['h1']}>Upcoming Events</h1>
+      <div className={styles.filters}>
+        <button onClick={() => handleCategoryChange('All')}>All</button>
+        <button onClick={() => handleCategoryChange('Concert')}>Concert</button>
+        <button onClick={() => handleCategoryChange('Conference')}>Conference</button>
+        <button onClick={() => handleCategoryChange('Exhibition')}>Exhibition</button>
+        {/* Add more category buttons */}
+      </div>
+      <div className={styles['event-list']}>
+        {filteredEvents.map(event => (
+          <div key={event.id} className={styles['event-card']}>
+            <h2>{event.title}</h2>
+            <p>{event.category}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default EventPage;
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,84 @@
 
-import AllEvents from "./allevents";
+// import AllEvents from "./allevents";
 
-import React from 'react'
+// import React from 'react'
 
-const index = () => {
+// const index = () => {
+//   return (
+//     <div>
+//         <AllEvents />
+//     </div>
+//   )
+// }
+
+// export default index
+
+
+import React, { useEffect, useState } from 'react';
+import styles from './Home.module.css'; // Import the CSS module
+import Card from './components/ui/Card';
+import { getAllData } from '@/helpers/api-handle';
+
+// const eventsData = [
+//   { id: 1, title: 'Event 1', category: 'Concert' },
+//   { id: 2, title: 'Event 2', category: 'Conference' },
+//   { id: 3, title: 'Event 3', category: 'Exhibition' },
+//   // Add more event data
+// ];
+
+const EventPage = () => {
+  const [selectedCategory, setSelectedCategory] = useState('Workshop');
+  const [eventData, seteventData] = useState([]);
+
+  // useEffect(() => {
+  //   const getdata = async () => {
+  //     const response = await getAllData();
+  //     seteventData(response)
+  //     console.log(eventData)
+  //     getdata()
+  //   }
+  // }, [])
+
+  useEffect(() => {
+    const getdata = async () => {
+      const response = await getAllData();
+      seteventData(response);
+      console.log(response); // Corrected this line
+    };
+
+    getdata(); // Moved the function call here
+  }, []);
+
+
+
+  const handleCategoryChange = (category) => {
+    setSelectedCategory(category);
+  };
+
+  const filteredEvents = selectedCategory === 'Art Workshop'
+    ? eventData
+    : eventData.filter(event => event.type === selectedCategory)
+  // .filter(event => event.type === selectedCategory);
   return (
-    <div>
-        <AllEvents />
-    </div>
-  )
-}
+    <div className={styles['event-page']}>
+      <h1 className={styles['h1']}>Upcoming Events</h1>
+      <div className={styles.filters}>
+        <button onClick={() => handleCategoryChange('Workshop')}>Art Workshop</button>
+        <button onClick={() => handleCategoryChange('Concert')}>Concert</button>
+        <button onClick={() => handleCategoryChange('Conference')}>Conference</button>
+        <button onClick={() => handleCategoryChange('Exhibition')}>Exhibition</button>
+        {/* Add more category buttons */}
+      </div>
+      <div >
+        {filteredEvents.map(event => (
 
-export default index
+          <Card event={event} />
+
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default EventPage;
+
